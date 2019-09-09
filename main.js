5da46adad8144910f3886950dcaaa5ee4c478d65
Fernando Lima <fernandojvlima@gmail.com>
2019-09-09 18:46:59 -0300

    Uploading Classes

--- main.js
+++ main.js
@@ -1,7 +1,38 @@
-alert('opa');
 
-class Teste {
-    metodo(){
-        
+//Working with classes
+class TodoList {
+    constructor(){
+        this.data = [];
+    }
+
+    add(data){
+        this.data.push(data);
+        console.log(this.data);
     }
 }
+
+class NewList extends TodoList {
+    constructor(){
+        super();
+        this.usuario = 'Diego';
+    }
+
+    mostraUsuario(){
+        console.log(this.usuario);
+    }
+}
+
+const MinhaLista = new NewList();
+
+document.getElementById('novo-todo').onclick = function() {
+    MinhaLista.add('novo todo');
+}
+
+
+class Math {
+    static soma(a,b){
+        return a+b;
+    } 
+}
+
+console.log(Math.soma(10,10));
